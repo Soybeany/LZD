57c2c21cc38aa85683fd9c9185e364e8dffd710d
Soybeany <521134470@qq.com>
2015-07-29 15:55:35 +0800

    test

---
+++ SSS/SSS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSS
+//
+//  Created by rusen02 on 15/7/18.
+//  Copyright (c) 2015年 rusen02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSS/SSS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSS
+//
+//  Created by rusen02 on 15/7/18.
+//  Copyright (c) 2015年 rusen02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
